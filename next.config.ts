1061fabf590245103fdae512608b89f0e2632751
Gabriel Machado <gabrielc.machado@hotmail.com>
2025-04-12 14:57:37 -0300

    next.config.ts configurado para github pages.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages não suporta otimização de imagem por default
+  },
+  trailingSlash: true, // importante para páginas exportadas funcionarem bem no GitHub Pages
   eslint: {
     ignoreDuringBuilds: true,
   },
